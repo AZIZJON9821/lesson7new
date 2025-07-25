314bb26b1d56b1f524657f65cf6656ed97fc45ae
AZIZ <bozorboyevazizjon56@gmail.com>
2025-07-25 16:02:04 +0500

    salom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // src/App.tsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 import UserList from './pages/UserList';
 import AddUser from './pages/AddUser';
 
